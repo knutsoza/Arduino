5b69ba37f7f310e364bd84efe2043a67736b2f0b
Zach Knutson <knutsoza@protonmail.ch>
2015-04-25 15:37:01 +0200

    Arduino_Projects

---
+++ LaunchConsol/LaunchConsol.ino
@@ -0,0 +1,35 @@
+
+//variables
+int switchstate = 0;
+
+//pin setup
+void setup() { 
+  pinMode(2,INPUT);
+  pinMode(3,OUTPUT),
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+}//setup
+
+//infiti loop
+void loop() {
+  if (switchstate == LOW){
+    //the button is not pressed
+    digitalWrite(3,HIGH);//red LED
+    digitalWrite(4,LOW);//blue LED
+    digitalWrite(5,LOW);//blue LED
+  }// if switchstate
+  else {//button is pressed
+    digitalWrite(3,LOW);
+    
+    digitalWrite(4,LOW);
+    digitalWrite(5,HIGH);
+    delay(250); //wait
+    //toggle LEDs
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+    
+    delay(250); //wait
+    
+  }//else
+
+}//loop()
